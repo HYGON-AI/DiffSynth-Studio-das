3ed0413b5b75fd975c944ae53fc3c0e5e6283d7a
iMountTai <2506700016@qq.com>
2026-09-21 09:34:30 +0800

    chore: retain license header for new AITER integration

--- diffsynth/models/minimax_h3_aiter.py
+++ diffsynth/models/minimax_h3_aiter.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2026 Hygon Information Technology Co., Ltd.
+# SPDX-License-Identifier: Apache-2.0
 """Optional AITER Add+SwiGLU for MiniMax-H3 PEFT fc1 adapters.
 
 No parameter replacement or global PEFT patching. All linear/dropout modules
